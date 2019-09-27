913b33cd9630f97049b784689beec0361061c798
Tomasz Pawlak <tomasz.pawlak@centre4.local>
2019-09-27 14:18:56 +0100

    Ver 2.0

--- jscript.js
+++ jscript.js
@@ -1,21 +1,39 @@
 $(document).ready(function()
 {
-    var Nname=document.querySelector('firstname');
-    var sName=document.quertySelector('surname');
-    var dateField=document.querySelector('date');
-    var timeField=document.querySelector('time');
-    var notes=document.querySelector('notes');
-    var form=document.querySelector('form');
-    var submit=document.querySelector('submit');
+    var db = openDatabase('C:\Users\Tomasz.Pawlak\Documents\IntroToDatabases\first_select_exercise\stuffed_face.db', '1.0', 'GP_DB', 2 * 1024 * 1024);
+   
+    var Nname=document.getElementById('firstname');
+    var sName=document.getElementById('surname');
+    var dataField=document.getElementById('date');
+    var timeField=document.getElementById('time');
+    var notes=document.getElementById('notes');
+    var form=document.getElementById('form');
+    var submit=document.getElementById('submit');
 
-
-    form.onsubmit=function(e)
+    function saveRecord()
     {
-        if (fName.value==='' || sName.value==='' || dateField.value==='' || timeField.value==='' )
-        {
-            e.preventDefault();
-            para.textContent='You need fill both fields !'; // extra Field for communicate !
-    
-        }
+
+        let userName=String(Nname.value);
+        //if
+        let userSurname=String(sName.value);
+        let data=dataField.value;
+        let time=timeField.value;
+        let extraNotes=notes.value;
+        console.log(userName);
+        console.log(userSurname);
+        console.log(data);
+        console.log(time);
+        
     }
+    submit.onclick=saveRecord;
+    
+    // form.onsubmit=function(e)
+    // {
+    //     if (fName.value==='' || sName.value==='' || dateField.value==='' || timeField.value==='' )
+    //     {
+    //         e.preventDefault();
+    //         para.textContent='You need fill both fields !'; // extra Field for communicate !
+    
+    //     }
+    // }
 });
